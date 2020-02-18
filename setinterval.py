39a383ba1061c85c11f3de7fafe46061a1a82547
Divyanshu Shekhar <61140213+divshekhar@users.noreply.github.com>
2020-02-18 09:19:52 +0530

    set interval deleted.

--- setinterval.py
+++
@@ -1,8 +0,0 @@
-import random
-# card = {}.fromkeys(['spade','diamond','heart','club'],['2','3','4','5','6','7','8','9','10','J','Q','K','A'])
-card = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
-card_types = ['spade','diamond','heart','club']
-random.shuffle(card)
-random.shuffle(card_types)
-print(card.pop())
-print(card_types.pop())
